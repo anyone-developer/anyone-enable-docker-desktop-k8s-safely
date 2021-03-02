baedbc286411965c563062dfb1856cb1f6f10b7d
Tekdycaml <zhang_nan_163@163.com>
2021-03-02 16:23:33 +0800

    Update load_images.bat
    修改bat脚本

--- load_images.bat
+++ load_images.bat
@@ -3,10 +3,8 @@ set file=images
 if exist %file% (
     echo %file% found.
     for /f "tokens=1,2 delims==" %%i in (%file%) do (
-        echo pulling %%j ...
-        docker pull %%j
-        docker tag %%j %%i
-        docker rmi %%j
+        echo pulling %%i ...
+        docker pull %%i
     )
 ) else (
     echo %file% not found.
